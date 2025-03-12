57d9e7bc0baa7a57ef4363a8744d2fa51096c9b5
MadLikeCoffee <aus05btitvas@gmail.com>
2025-03-12 20:27:36 +0200

    trying

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('dark-light')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  document.body.classList.remove('lightmode')
+
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  document.body.classList.add('lightmode')
+
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
