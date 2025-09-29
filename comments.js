b711df859a9de6b6f83790bfdcd6d11699614d81
MadLikeCoffee <aus05btitvas@gmail.com>
2025-09-29 23:03:41 +0300

    Comments way shorter code

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-const asd = document.getElementsByClassName('comment-container')
+/*const asd = document.getElementsByClassName('comment-container')*/
 
 /*
 const parent = document.getElementsByClassName('comment-reply')
@@ -6,10 +6,14 @@ console.log(parent.item(0).parentElement.children.item(3))
 console.log(parent.item(0).parentElement.children.item(parent.item(0).parentElement.children.length-1))
 console.log(parent.item(0).parentElement.children.item(3))
 */
+
+
+
+/*
 for(let i = 0; i <= asd.length -1; i++ ){
     asd.item(i).children.item(1).addEventListener("click", () => {
     if(asd.item(i).children.item(asd.item(i).children.length-1).classList.contains('hidden') == true){
-        /*console.log('contains')*/
+        //console.log('contains')
         asd.item(i).children.item(asd.item(i).children.length-1).classList.remove('hidden')
     }
     else if(asd.item(i).children.item(asd.item(i).children.length-1).classList.contains('hidden') == false){
@@ -17,4 +21,23 @@ for(let i = 0; i <= asd.length -1; i++ ){
     }
     
 })
+}
+*/
+
+
+function commentsection(e){
+    //console.log(e)
+    //console.log(e.parentElement)
+    //console.log(e.parentElement.children)
+    //console.log(e.parentElement.children.item(3))
+    //console.log(e.parentElement.children.item(3).classList.contains('hidden'))
+    //e.parentElement.children.item(3).classList.remove('hidden')
+
+    if(e.parentElement.children.item(e.parentElement.children.length-1).classList.contains('hidden') == true){
+        e.parentElement.children.item(e.parentElement.children.length-1).classList.remove('hidden')
+    }
+    else if(e.parentElement.children.item(e.parentElement.children.length-1).classList.contains('hidden') == false){
+        e.parentElement.children.item(e.parentElement.children.length-1).classList.add('hidden')
+    }
+
 }
