53d809f295530c15f2b2b15d76e27be7f3b4530f
MadLikeCoffee <aus05btitvas@gmail.com>
2025-09-29 22:41:47 +0300

    Comments ideas and rps delay on button press

--- comments.js
+++ comments.js
@@ -1,14 +1,19 @@
 const asd = document.getElementsByClassName('comment-container')
 
-
+/*
+const parent = document.getElementsByClassName('comment-reply')
+console.log(parent.item(0).parentElement.children.item(3))
+console.log(parent.item(0).parentElement.children.item(parent.item(0).parentElement.children.length-1))
+console.log(parent.item(0).parentElement.children.item(3))
+*/
 for(let i = 0; i <= asd.length -1; i++ ){
     asd.item(i).children.item(1).addEventListener("click", () => {
-    if(asd.item(i).children.item(3).classList.contains('hidden') == true){
+    if(asd.item(i).children.item(asd.item(i).children.length-1).classList.contains('hidden') == true){
         /*console.log('contains')*/
-        asd.item(i).children.item(3).classList.remove('hidden')
+        asd.item(i).children.item(asd.item(i).children.length-1).classList.remove('hidden')
     }
-    else if(asd.item(i).children.item(3).classList.contains('hidden') == false){
-        asd.item(i).children.item(3).classList.add('hidden')
+    else if(asd.item(i).children.item(asd.item(i).children.length-1).classList.contains('hidden') == false){
+        asd.item(i).children.item(asd.item(i).children.length-1).classList.add('hidden')
     }
     
 })
--- sbrps.js
+++ sbrps.js
@@ -6,15 +6,14 @@ const robotpick = document.getElementById("robot-pick");
 const gamestate = document.getElementById("game-state");
 const volumedialrps = document.getElementById("volumedialrps");
 
+var volumedialrpschanged = localStorage.getItem("volume");
+var volumedialmemory = localStorage.getItem("volumememory");
 
-var volumedialrpschanged = localStorage.getItem('volume')
-var volumedialmemory = localStorage.getItem('volumememory')
+var disabled;
 
-
-
-var win = new Audio('Audio/win.wav');
-var lose = new Audio('Audio/lose.wav');
-var tie = new Audio('Audio/tie.wav');
+var win = new Audio("Audio/win.wav");
+var lose = new Audio("Audio/lose.wav");
+var tie = new Audio("Audio/tie.wav");
 
 /*if(volumedialmemory == null){
     volumedialrps.setAttribute("value", 25);
@@ -34,102 +33,129 @@ else {
 }
 */
 // ??= operator is used if value is null or undefined set value
-    volumedialmemory ??= 25;
-    volumedialrps.setAttribute("value", volumedialmemory);
-    volumedialrpschanged ??= 0.25;
+volumedialmemory ??= 25;
+volumedialrps.setAttribute("value", volumedialmemory);
+volumedialrpschanged ??= 0.25;
 
-    //console.log(volumedialrpschanged);
-    //console.log(volumedialrps.getAttribute("value"));
+//console.log(volumedialrpschanged);
+//console.log(volumedialrps.getAttribute("value"));
 
-    win.volume = volumedialrpschanged;
-    lose.volume = volumedialrpschanged;
-    tie.volume = volumedialrpschanged;
+win.volume = volumedialrpschanged;
+lose.volume = volumedialrpschanged;
+tie.volume = volumedialrpschanged;
 
-volumedialrps.oninput = function() {
-    volumedialrpschanged = this.value / 100;
-    volumedialmemory = this.value;
-    //console.log(volumedialrpschanged);
+volumedialrps.oninput = function () {
+  volumedialrpschanged = this.value / 100;
+  volumedialmemory = this.value;
+  //console.log(volumedialrpschanged);
 
-    win.volume = volumedialrpschanged;
-    lose.volume = volumedialrpschanged;
-    tie.volume = volumedialrpschanged;
-    
-    localStorage.setItem('volume', volumedialrpschanged);
-    
-    localStorage.setItem('volumememory', volumedialmemory);
-}
+  win.volume = volumedialrpschanged;
+  lose.volume = volumedialrpschanged;
+  tie.volume = volumedialrpschanged;
 
+  localStorage.setItem("volume", volumedialrpschanged);
 
-function randomIntFromInterval(min, max) { // min and max included 
+  localStorage.setItem("volumememory", volumedialmemory);
+};
+
+function randomIntFromInterval(min, max) {
+  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
- let randomas
- let pasirinkimas
+let randomas;
+let pasirinkimas;
 
-function robotas(randomas){
-    // Robotas ranka
-    if (randomas === 1) {
-        robotpick.classList.remove("fa-hand-fist");
-        robotpick.classList.remove("fa-hand-scissors");
-        robotpick.classList.add("fa-hand");
-    }
-    // Robotas akmuo
-    if (randomas === 2){
-        robotpick.classList.remove("fa-hand");
-        robotpick.classList.remove("fa-hand-scissors");
-        robotpick.classList.add("fa-hand-fist");
-    }
-    // Robotas žirklės
-    if (randomas === 3){
-        robotpick.classList.remove("fa-hand-fist");
-        robotpick.classList.remove("fa-hand");
-        robotpick.classList.add("fa-hand-scissors");
-    }
+function robotas(randomas) {
+  // Robotas ranka
+  if (randomas === 1) {
+    robotpick.classList.remove("fa-hand-fist");
+    robotpick.classList.remove("fa-hand-scissors");
+    robotpick.classList.add("fa-hand");
+  }
+  // Robotas akmuo
+  if (randomas === 2) {
+    robotpick.classList.remove("fa-hand");
+    robotpick.classList.remove("fa-hand-scissors");
+    robotpick.classList.add("fa-hand-fist");
+  }
+  // Robotas žirklės
+  if (randomas === 3) {
+    robotpick.classList.remove("fa-hand-fist");
+    robotpick.classList.remove("fa-hand");
+    robotpick.classList.add("fa-hand-scissors");
+  }
 }
-function decider(randomas, pasirinkimas){
-    if (pasirinkimas === randomas){
-        gamestate.textContent="Lygiosios";
-        tie.load();
-        tie.play();
-    }
-    if ((pasirinkimas === 1 && randomas === 2) || (pasirinkimas === 2 && randomas === 3) || (pasirinkimas === 3 && randomas === 1)){
-        gamestate.textContent="Laimėjai!!";
-        win.load();
-        win.play();
-    }
-    if ((pasirinkimas === 1 && randomas === 3) || (pasirinkimas === 2 && randomas === 1) || (pasirinkimas === 3 && randomas === 2)){
-        gamestate.textContent="Pralaimėjai!!";
-        lose.load();
-        lose.play();
-    }
-
+function decider(randomas, pasirinkimas) {
+  if (pasirinkimas === randomas) {
+    gamestate.textContent = "Lygiosios";
+    tie.load();
+    tie.play();
+  }
+  if (
+    (pasirinkimas === 1 && randomas === 2) ||
+    (pasirinkimas === 2 && randomas === 3) ||
+    (pasirinkimas === 3 && randomas === 1)
+  ) {
+    gamestate.textContent = "Laimėjai!!";
+    win.load();
+    win.play();
+  }
+  if (
+    (pasirinkimas === 1 && randomas === 3) ||
+    (pasirinkimas === 2 && randomas === 1) ||
+    (pasirinkimas === 3 && randomas === 2)
+  ) {
+    gamestate.textContent = "Pralaimėjai!!";
+    lose.load();
+    lose.play();
+  }
 }
 
 paperbtn.addEventListener("click", () => {
-    
-  playerpick.classList.remove("fa-hand-fist");
-  playerpick.classList.remove("fa-hand-scissors");
-  playerpick.classList.add("fa-hand");
-  randomas = randomIntFromInterval(1, 3);
-  pasirinkimas = 1;
-    robotas(randomas)
-    decider(randomas, pasirinkimas)
-})
+  if (disabled != true) {
+    playerpick.classList.remove("fa-hand-fist");
+    playerpick.classList.remove("fa-hand-scissors");
+    playerpick.classList.add("fa-hand");
+    randomas = randomIntFromInterval(1, 3);
+    pasirinkimas = 1;
+    robotas(randomas);
+    decider(randomas, pasirinkimas);
+    disabled = true;
+    setTimeout(() => {
+      disabled = false;
+      console.log("timedout");
+    }, 1000);
+  }
+});
 rockbtn.addEventListener("click", () => {
-  playerpick.classList.remove("fa-hand");
-  playerpick.classList.remove("fa-hand-scissors");
-  playerpick.classList.add("fa-hand-fist");
-  randomas = randomIntFromInterval(1, 3);
-  pasirinkimas = 2;
-    robotas(randomas)
-    decider(randomas, pasirinkimas)
-})
+  if (disabled != true) {
+    playerpick.classList.remove("fa-hand");
+    playerpick.classList.remove("fa-hand-scissors");
+    playerpick.classList.add("fa-hand-fist");
+    randomas = randomIntFromInterval(1, 3);
+    pasirinkimas = 2;
+    robotas(randomas);
+    decider(randomas, pasirinkimas);
+    disabled = true;
+    setTimeout(() => {
+      disabled = false;
+      console.log("timedout");
+    }, 1000);
+  }
+});
 scissorsbtn.addEventListener("click", () => {
-  playerpick.classList.remove("fa-hand-fist");
-  playerpick.classList.remove("fa-hand");
-  playerpick.classList.add("fa-hand-scissors");
-  randomas = randomIntFromInterval(1, 3);
-  pasirinkimas = 3;
-    robotas(randomas)
-    decider(randomas, pasirinkimas)
-})
+  if (disabled != true) {
+    playerpick.classList.remove("fa-hand-fist");
+    playerpick.classList.remove("fa-hand");
+    playerpick.classList.add("fa-hand-scissors");
+    randomas = randomIntFromInterval(1, 3);
+    pasirinkimas = 3;
+    robotas(randomas);
+    decider(randomas, pasirinkimas);
+    disabled = true;
+    setTimeout(() => {
+      disabled = false;
+      console.log("timedout");
+    }, 1000);
+  }
+});
